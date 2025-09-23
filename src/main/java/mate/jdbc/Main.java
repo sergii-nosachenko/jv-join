1b739e74a69765268fa680ab4187d93d08d003bd
Sergii Nosachenko <sergii.n@mate.academy>
2025-09-23 17:50:29 +0300

    Add DAO and model imports in Main.java
    Added imports for DAO implementations and models.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,5 +1,19 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.CarDao;
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.dao.impl.CarDaoImpl;
+import mate.jdbc.dao.impl.DriverDaoImpl;
+import mate.jdbc.dao.impl.ManufacturerDaoImpl;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Car;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.*;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // test your code here
